4483d3b8b87b9eeb39be847d255eda874c4eb7da
Candyan <liuyanhp@gmail.com>
2015-01-21 16:57:23 +0800

    add dispatch_main_sync & asyn define

--- Source/PACommonFunctions.h
+++ Source/PACommonFunctions.h
@@ -9,23 +9,39 @@
 #ifndef PupaDemo_PACommonFunctions_h
 #define PupaDemo_PACommonFunctions_h
 
-#ifndef PALocalString(__KEY)
+#ifndef PALocalString
 #define PALocalString(__KEY) NSLocalizedString(__KEY, nil)
 #endif
 
 #if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_6_1
-#ifndef IF_IOS7_OR_GREATER(...)
-#define IF_IOS7_OR_GREATER(...) \
-if (kCFCoreFoundationVersionNumber > kCFCoreFoundationVersionNumber_iOS_6_1) \
-{ \
-__VA_ARGS__ \
-}
+#ifndef IF_IOS7_OR_GREATER
+#define IF_IOS7_OR_GREATER(...)                                                                                        \
+    if (kCFCoreFoundationVersionNumber > kCFCoreFoundationVersionNumber_iOS_6_1) {                                     \
+        __VA_ARGS__                                                                                                    \
+    }
 #endif
 #else
-#ifndef IF_IOS7_OR_GREATER(...)
+#ifndef IF_IOS7_OR_GREATER
 #define IF_IOS7_OR_GREATER(...)
 #endif
 #endif
 
+#ifndef dispatch_main_sync_safe
+#define dispatch_main_sync_safe(block)                                                                                 \
+    if ([NSThread isMainThread]) {                                                                                     \
+        block();                                                                                                       \
+    } else {                                                                                                           \
+        dispatch_sync(dispatch_get_main_queue(), block);                                                               \
+    }
+#endif
+
+#ifndef dispatch_main_async_safe
+#define dispatch_main_async_safe(block)                                                                                \
+    if ([NSThread isMainThread]) {                                                                                     \
+        block();                                                                                                       \
+    } else {                                                                                                           \
+        dispatch_async(dispatch_get_main_queue(), block);                                                              \
+    }
+#endif
 
 #endif
